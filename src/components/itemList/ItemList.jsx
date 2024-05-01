f29e00509337d7ffbd377a7242b7b16b6bee15d7
Nicolas Cordero <nicocordero@gmail.com>
2024-05-01 20:10:04 -0300

    Mejora whatsapp

--- src/components/itemList/ItemList.jsx
+++ src/components/itemList/ItemList.jsx
@@ -1,13 +1,14 @@
 import Item from "../item/Item"
+import { Link } from 'react-router-dom';
 import './itemList.css'
 
 const ItemList = ({ productos }) => {
     return (
         <div className="item-list">
             {productos.map((producto) => <Item key={producto.id} producto={producto} />)}
-            <a className="whatsapp-link" href="https://web.whatsapp.com/send?phone=543364312059" target="_blank" rel="noopener noreferrer">
-                        <img className="whatsapp-logo" src="../images/whatsapplogo.png" alt="WhatsApp" />
-                    </a>
+            <Link className="whatsapp-link" to="https://web.whatsapp.com/send?phone=5493364312059" target="_blank" rel="noopener noreferrer">
+                <img className="whatsapp-logo" src="../images/whatsapplogo.png" alt="WhatsApp" />
+            </Link>
         </div>
     )
 }
