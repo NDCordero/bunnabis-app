a6e88977e193260ac89bfe2b779b2a098806d04c
Nicolas Cordero <nicocordero@gmail.com>
2024-05-01 20:03:31 -0300

    Link a whatsapp

--- src/components/itemList/ItemList.jsx
+++ src/components/itemList/ItemList.jsx
@@ -5,6 +5,9 @@ const ItemList = ({ productos }) => {
     return (
         <div className="item-list">
             {productos.map((producto) => <Item key={producto.id} producto={producto} />)}
+            <a className="whatsapp-link" href="https://web.whatsapp.com/send?phone=543364312059" target="_blank" rel="noopener noreferrer">
+                        <img className="whatsapp-logo" src="../images/whatsapplogo.png" alt="WhatsApp" />
+                    </a>
         </div>
     )
 }
