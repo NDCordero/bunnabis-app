0d2833620e76d0409798b04f4b94c813fc9f0793
Nicolas Cordero <nicocordero@gmail.com>
2024-05-02 19:48:37 -0300

    Correccion whatsapp

--- src/components/buttonWhatsApp/ButtonWhatsApp.jsx
+++ src/components/buttonWhatsApp/ButtonWhatsApp.jsx
@@ -2,25 +2,25 @@ import React from 'react';
 
 
 const WhatsAppButton = () => {
-  const phoneNumber = '+5493364312059'; // Tu número de WhatsApp
+  const phoneNumber = '+5493364312059';
 
   const handleWhatsAppClick = () => {
-    // Definir el enlace de WhatsApp
+
     let whatsappLink = `https://api.whatsapp.com/send?phone=${phoneNumber}`;
 
-    // Si es una versión de escritorio, redirige a WhatsApp Web
+   
     if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
       whatsappLink = `https://web.whatsapp.com/send?phone=${phoneNumber}`;
     }
 
-    // Abrir el enlace en una nueva ventana
+
     window.open(whatsappLink, '_blank');
   };
 
   return (
     <div>
-      <button className='whatsapp-link' onClick={handleWhatsAppClick}>
-        <img className='whatsapp-logo' src="../images/whatsapplogo.png" alt="Contactanos por WhatsApp" />
+      <button onClick={handleWhatsAppClick}>
+        <img className='whatsapp-logo whatsapp-link' src="../images/whatsapplogo.png" alt="Contactanos por WhatsApp" />
       </button>
     </div>
   );
--- src/components/itemList/ItemList.jsx
+++ src/components/itemList/ItemList.jsx
@@ -1,5 +1,4 @@
 import Item from "../item/Item"
-import { Link } from 'react-router-dom';
 import './itemList.css';
 import ButtonWhatsApp from "../buttonWhatsApp/ButtonWhatsApp";
 
@@ -8,9 +7,6 @@ const ItemList = ({ productos }) => {
     return (
         <div className="item-list">
             {productos.map((producto) => <Item key={producto.id} producto={producto} />)}
-            {/* {<Link className="whatsapp-link" to="https://api.whatsapp.com/send?phone=5493364312059" target="_blank" rel="noopener noreferrer">
-                <img className="whatsapp-logo" src="../images/whatsapplogo.png" alt="Contactanos por WhatsApp" />
-            </Link>} */}
             {<ButtonWhatsApp />}
         </div>
     )
